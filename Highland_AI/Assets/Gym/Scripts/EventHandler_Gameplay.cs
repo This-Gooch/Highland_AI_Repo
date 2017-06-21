eb8819cd31925c069422b1360924d029915718d5
Leonard <leonardquintal@gmail.com>
2017-06-21 12:22:40 -0400

    Added base event class for units

---
+++ Highland_AI/Assets/Gym/Scripts/EventHandler_Gameplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Event delegate relating to Gameplay.
+/// Implementation is done on the registered entity.
+/// </summary>
+public class EventHandler_Gameplay : MonoBehaviour {
+    
+    //Delegate 
+    public delegate void UnitEventHandler(GameObject unit);
+    //OnUnitSpawn event. Register objects to implement function On unit spawn.
+    public static event UnitEventHandler OnUnitSpawn;
+    //OnUnitDestroyed event. Register objects to implement functions On unit destroyed.
+    public static event UnitEventHandler OnUnitDestroyed;
+
+    //Calls the delegate
+    public static void NewUnitCreated(GameObject unit)
+    {
+        if (OnUnitSpawn != null)
+        {
+            OnUnitSpawn(unit);
+        }
+    }
+    //Calls the delegate
+    public static void UnitDead(GameObject unit)
+    {
+        if (OnUnitDestroyed != null)
+        {
+            OnUnitDestroyed(unit);
+        }
+    }
+}
---
+++ Highland_AI/Assets/Gym/Scripts/Unit.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+
+    private void Awake()
+    {
+    }
+
+    private void Start()
+    {
+        //Register all eventhandlers on start.
+        //This is mostly an example of usage at the momment.
+        EventHandler_Gameplay.OnUnitSpawn += this.NewUnitCreated;
+        EventHandler_Gameplay.OnUnitDestroyed += this.OnUnitDestroy;
+        //Trigger new unit created.
+        EventHandler_Gameplay.NewUnitCreated(this.gameObject);
+    }
+
+    void Update()
+    {
+
+    }
+
+    #region Events
+    public void KillUnit()
+    {
+        //Trigger unit death
+        EventHandler_Gameplay.UnitDead(this.gameObject);
+    }
+    //Implementation of the event.
+    private void OnUnitDestroy(GameObject unit)
+    {
+        //unregister all handlers if we remove the unit from the game.
+        EventHandler_Gameplay.OnUnitSpawn -= this.NewUnitCreated;
+        EventHandler_Gameplay.OnUnitDestroyed -= this.OnUnitDestroy;
+    }
+    //Implementation of the event.
+    private void NewUnitCreated(GameObject unit)
+    {
+        console.log("New Unit created: " + gameObject.name);
+    }
+
+    #endregion
+
+
+}
