2879c6b1cc419688d5c6c4994a81a5c3263e91fd
Leonard <leonardquintal@gmail.com>
2017-07-01 11:19:24 -0400

    pre meeting

--- Highland_AI/Assets/Gym/Scripts/NSGameplay.cs
+++ Highland_AI/Assets/Gym/Scripts/NSGameplay.cs
@@ -67,38 +67,96 @@
             ///         0 - 1000                                        //
             /// Class 1:                                                //
             ///     reserved values:                                    //
-            ///         2000 - 3000
-            /// Class 2:
-            ///     reserved values:
-            ///         4000 - 5000
-            /// Class 3:
-            ///     reserved values:
-            ///         6000 - 7000
-            /// Class 4:
-            ///     reserved values:
-            ///         8000 - 9000
-            /// Class 5:
-            ///     reserved values:
-            ///         10,000 - 11,000
-            /// Class 6:
-            ///     reserved values:
-            ///         12,000 - 13,000
-            /// Class 8:
-            ///     reserved values:
-            ///         14,000 - 15,000
-            /// 
+            ///         2000 - 3000                                     //
+            /// Class 2:                                                //
+            ///     reserved values:                                    //
+            ///         4000 - 5000                                     //
+            /// Class 3:                                                //
+            ///     reserved values:                                    //
+            ///         6000 - 7000                                     //
+            /// Class 4:                                                //
+            ///     reserved values:                                    //
+            ///         8000 - 9000                                     //
+            /// Class 5:                                                //
+            ///     reserved values:                                    //
+            ///         10,000 - 11,000                                 //
+            /// Class 6:                                                //
+            ///     reserved values:                                    //
+            ///         12,000 - 13,000                                 //
+            /// Class 7:                                                //
+            ///     reserved values:                                    //
+            ///         14,000 - 15,000                                 //
+            /// Class 8:                                                //
+            ///     reserved values:                                    //
+            ///         16,000 - 17,000                                 //
+            ///                                                         //
             //////////////////////////////////////////////////////////////
 
-            ///Standard Set
-            ///Card all units/classes can use in their decks.
-            ///Reversed from 0-
-            ///
-            ///
 
-            DirectAttack = 2147483647
 
+            ///********************************************************///
+            /// Standard Set                                           ///
+            ///Card all units/classes can use in their decks.          ///
+            ///Reversed from 0 - 1000                                  ///
+            ///********************************************************///
+
+            DirectAttack = 0,
+            Guard = 100,
+
+            ShuffleDeck = 200
+
+            ///********************************************************///
+            /// Class 1 Set                                            ///
+            ///xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx          ///
+            ///Reversed from 2000 - 3000                               ///
+            ///********************************************************///
+
+            ///********************************************************///
+            /// Class 2 Set                                            ///
+            ///xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx          ///
+            ///Reversed from 4000 - 5000                               ///
+            ///********************************************************///        
+
+            ///********************************************************///
+            /// Class 3 Set                                            ///
+            ///xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx          ///
+            ///Reversed from 6000 - 7000                               ///
+            ///********************************************************///
+
+
+            ///********************************************************///
+            /// Class 4 Set                                            ///
+            ///xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx          ///
+            ///Reversed from 8000 - 9000                               ///
+            ///********************************************************///
+
+            ///********************************************************///
+            /// Class 5 Set                                            ///
+            ///xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx          ///
+            ///Reversed from 10000 - 11000                             ///
+            ///********************************************************///
+
+
+            ///********************************************************///
+            /// Class 6 Set                                            ///
+            ///xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx          ///
+            ///Reversed from 12000 - 13000                             ///
+            ///********************************************************///
+
+            ///********************************************************///
+            /// Class 7 Set                                            ///
+            ///xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx          ///
+            ///Reversed from 14000 - 15000                             ///
+            ///********************************************************///
+
+            ///********************************************************///
+            /// Class 8 Set                                            ///
+            ///xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx          ///
+            ///Reversed from 16000 - 17000                             ///
+            ///********************************************************///
         }
-        
+
+
     }
 
     //State machine related namespace.
